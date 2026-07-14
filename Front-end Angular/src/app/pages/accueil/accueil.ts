a7008d69db832f8e2b191d405f0fed204faa0d39
Yoel ILLOUZ <yoel.illouz@alten.com>
2026-07-14 21:05:09 +0200

    ajout de la suite de la page d'accueil

--- Front-end Angular/src/app/pages/accueil/accueil.ts
+++ Front-end Angular/src/app/pages/accueil/accueil.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { EspacePetit } from "../../components/components-espaces/components-espaces/espace-petit/espace-petit";
 import { ComponentAccueil1 } from "../../components/components-reutilisables/component-accueil1/component-accueil1";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-accueil',
-  imports: [MatCardModule, EspacePetit, ComponentAccueil1],
+  imports: [
+    MatCardModule, 
+    EspacePetit, 
+    ComponentAccueil1,
+    MatButtonModule
+  ],
   templateUrl: './accueil.html',
   styleUrl: './accueil.scss',
 })
