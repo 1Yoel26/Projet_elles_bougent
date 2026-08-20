cd945d402fb16cb8336e01741d2852ed43b7d45e
Yoel ILLOUZ <yoel.illouz@alten.com>
2026-08-20 19:03:29 +0200

    ajout de la suite2 du menu version ordinateur

--- Front-end Angular/src/app/components/menu/menu-principal-ordinateur/menu-principal-ordinateur.ts
+++ Front-end Angular/src/app/components/menu/menu-principal-ordinateur/menu-principal-ordinateur.ts
@@ -1,64 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { SousMenuPrincipal } from '../../../interfaces/sous-menu-principal';
+import { NgIf } from '@angular/common';
+import { dataMenuOrdinateur } from '../../../data-statique/menu-ordinateur';
 
 @Component({
   selector: 'app-menu-principal-ordinateur',
-  imports: [MatToolbarModule, RouterLink, MatButtonModule],
+  imports: [MatToolbarModule, RouterLink, MatButtonModule, NgIf],
   templateUrl: './menu-principal-ordinateur.html',
   styleUrl: './menu-principal-ordinateur.scss',
 })
-export class MenuPrincipalOrdinateur implements OnInit{
+export class MenuPrincipalOrdinateur {
+
+  constructor(private cdr: ChangeDetectorRef){}
+
 
   public contenuSousMenu!: SousMenuPrincipal;
-  lienSurvoler: string | null = "delegation";
+  lienEstSurvoler: string | null = null;
+  listeDesLiensASurvoler: string[] = ["association", "actions", "delegation", "engagement"];
 
 
-  ngOnInit(): void {
+  public lienSurvoler(lien: string){
+    
+    // recuperation du lien survoler parmis les 4 liens qui on l'evenement pour detecter le (mouseenter):
+    this.lienEstSurvoler = lien;
 
-    if(this.lienSurvoler && this.lienSurvoler == "delegation"){
+    // si le lien survoler fait bien partie de la listeDesLiensASurvoler,
+    // alors on charge les données pour le contenuSousMenu (qui sera affiché dans le menu-ordinateur.html):
+    if(this.lienEstSurvoler != null && this.listeDesLiensASurvoler.includes(this.lienEstSurvoler)){
+      
+      this.contenuSousMenu = dataMenuOrdinateur[this.lienEstSurvoler];
+      
+    }
+    // si le lien récupérer n'est pas valide (par mesure de sécurité):
+    else{
 
-      this.contenuSousMenu = {
-        titrePresentation1: "Présentation",
-        paragraphePresentation1: "Elles bougent œuvre pour renforcer la mixité dans les secteurs scientifiques, technologiques et industriels, en donnant aux jeunes filles les clés pour s’orienter vers les métiers d’ingénieure et de technicienne.",
-        tableauTitreBouton: [
-          "En savoir plus"
-        ],
-        listeNomsImages: [
-          {
-            titreImage: "En France",
-            cheminImage: "images/images-menu/subnav-france.jpg"
-          },
-
-          {
-            titreImage: "À l'international",
-            cheminImage: "images/images-menu/subnav-international.jpg"
-          },
-
-          {
-            titreImage: "En France",
-            cheminImage: "images/images-menu/subnav-france.jpg"
-          },
-
-          {
-            titreImage: "À l'international",
-            cheminImage: "images/images-menu/subnav-international.jpg"
-          },
-        ],
-
-        titrePresentation2: "Devenez déléguée régionale !",
-        paragraphePresentation2: "Coordonnez nos actions, mobilisez le réseau local et inspirez les filles sur votre territoire.",
-        titreBouton2: "Demande d'information"
-
-      }
-
+      this.lienEstSurvoler = null;
 
     }
 
+  }
 
+  // cette fonction detecte lorsque la souris sors du sous-menu, avec le (mouseleave) dans le fichier .html):
+  public sousMenuQuitter(){
+
+    this.lienEstSurvoler = null;
     
+  }
+
+  public fermerSousMenuAvecDelais(){
+
+    setTimeout(() => {
+
+      this.lienEstSurvoler = null;
+      alert(this.lienEstSurvoler);
+
+      this.cdr.detectChanges();
+      
+    }, 2000);
+    
+
   }
 
 }
---
+++ Front-end Angular/src/app/data-statique/menu-ordinateur.ts
@@ -0,0 +1,137 @@
+import { SousMenuPrincipal } from "../interfaces/sous-menu-principal";
+
+export const dataMenuOrdinateur: Record<string, SousMenuPrincipal> = {
+
+    "association": {
+
+        titrePresentation1: "Présentation",
+        paragraphePresentation1: "Elles bougent œuvre pour renforcer la mixité dans les secteurs scientifiques, technologiques et industriels, en donnant aux jeunes filles les clés pour s’orienter vers les métiers d’ingénieure et de technicienne.",
+        tableauTitreBouton: [
+          "En savoir plus"
+        ],
+        listeNomsImages: [
+          {
+            titreImage: "Nos missions",
+            cheminImage: "images/images-menu/subnav-chiffres.jpg"
+          },
+
+          {
+            titreImage: "Organisation",
+            cheminImage: "images/images-menu/subnav-equipe.jpg"
+          },
+
+          {
+            titreImage: "Chiffres clés",
+            cheminImage: "images/images-menu/subnav-chiffres.jpg"
+          }
+
+        ],
+
+        titrePresentation2: "Taxe d'apprentissage",
+        paragraphePresentation2: "L'association Elles bougent est habilitée à percevoir la taxe d'apprentissage.",
+        titreBouton2: "En savoir plus"
+
+    },
+
+    "actions": {
+
+        titrePresentation1: "Nos actions",
+        paragraphePresentation1: "Elles bougent œuvre pour renforcer la mixité dans les secteurs scientifiques, technologiques et industriels, en donnant aux jeunes filles les clés pour s’orienter vers les métiers d’ingénieure et de technicienne.",
+        tableauTitreBouton: [
+          "En savoir plus"
+        ],
+        listeNomsImages: [
+          {
+            titreImage: "En France",
+            cheminImage: "images/images-menu/subnav-france.jpg"
+          },
+
+          {
+            titreImage: "À l'international",
+            cheminImage: "images/images-menu/subnav-international.jpg"
+          },
+
+          {
+            titreImage: "En France",
+            cheminImage: "images/images-menu/subnav-france.jpg"
+          },
+
+          {
+            titreImage: "À l'international",
+            cheminImage: "images/images-menu/subnav-international.jpg"
+          },
+        ],
+
+        titrePresentation2: "Devenez déléguée régionale !",
+        paragraphePresentation2: "Coordonnez nos actions, mobilisez le réseau local et inspirez les filles sur votre territoire.",
+        titreBouton2: "Demande d'information"
+
+    },
+
+    "delegation": {
+
+        titrePresentation1: "Nos délégations",
+        paragraphePresentation1: "Elles bougent œuvre pour renforcer la mixité dans les secteurs scientifiques, technologiques et industriels, en donnant aux jeunes filles les clés pour s’orienter vers les métiers d’ingénieure et de technicienne.",
+        tableauTitreBouton: [
+          "En savoir plus"
+        ],
+        listeNomsImages: [
+          {
+            titreImage: "Nos missions",
+            cheminImage: "images/images-menu/subnav-chiffres.jpg"
+          },
+
+          {
+            titreImage: "Organisation",
+            cheminImage: "images/images-menu/subnav-equipe.jpg"
+          },
+
+          {
+            titreImage: "Chiffres clés",
+            cheminImage: "images/images-menu/subnav-chiffres.jpg"
+          }
+
+        ],
+
+        titrePresentation2: "Taxe d'apprentissage",
+        paragraphePresentation2: "L'association Elles bougent est habilitée à percevoir la taxe d'apprentissage.",
+        titreBouton2: "En savoir plus"
+
+    },
+
+    engagement : {
+
+        titrePresentation1: "Nos engagements",
+        paragraphePresentation1: "Elles bougent œuvre pour renforcer la mixité dans les secteurs scientifiques, technologiques et industriels, en donnant aux jeunes filles les clés pour s’orienter vers les métiers d’ingénieure et de technicienne.",
+        tableauTitreBouton: [
+          "En savoir plus"
+        ],
+        listeNomsImages: [
+          {
+            titreImage: "En France",
+            cheminImage: "images/images-menu/subnav-france.jpg"
+          },
+
+          {
+            titreImage: "À l'international",
+            cheminImage: "images/images-menu/subnav-international.jpg"
+          },
+
+          {
+            titreImage: "En France",
+            cheminImage: "images/images-menu/subnav-france.jpg"
+          },
+
+          {
+            titreImage: "À l'international",
+            cheminImage: "images/images-menu/subnav-international.jpg"
+          },
+        ],
+
+        titrePresentation2: "Devenez déléguée régionale !",
+        paragraphePresentation2: "Coordonnez nos actions, mobilisez le réseau local et inspirez les filles sur votre territoire.",
+        titreBouton2: "Demande d'information"
+
+    }
+
+}
